edb689b7da35dff27f6d5b91b5505b468c803bd2
Marcos Garcia de Lima <marckfree@gmail.com>
2021-01-10 12:14:37 -0300

    Add message

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('teste')
+const qrcode = require('qrcode-terminal')
+
+const { Client } = require('whatsapp-web.js')
+const client = new Client()
+
+client.on('qr', qr => {
+  qrcode.generate(qr, { small: true })
+})
+
+client.on('ready', () => {
+  console.log('Client is ready!')
+})
+
+client.on('message', message => {
+  if (message.body.includes('hora') || message.body.includes('horas')) {
+    message.reply(`São ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`)
+  }
+})
+
+client.initialize()
